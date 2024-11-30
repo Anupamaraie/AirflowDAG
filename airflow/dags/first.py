80ee724b2228001a69f38d643db3f8b0e35ac3cd
Anupamaraie <anuraie1@gmail.com>
2024-11-30 17:40:39 +0545

    success

--- airflow/dags/first.py
+++ airflow/dags/first.py
@@ -3,81 +3,49 @@ from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime
 import requests
+import logging
+import random
 
+quotes = ['What a good day to be alive!!', 'Expectations hurt!!', 'You are strong!!']
 
+# Logging setup
+logging.basicConfig(level=logging.INFO)
 
 def print_welcome():
-
-    print('Welcome to Airflow!')
-
-
+    logging.info('Welcome to Airflow!')
 
 def print_date():
-
-    print('Today is {}'.format(datetime.today().date()))
-
-
+    logging.info('Today is {}'.format(datetime.today().date()))
 
 def print_random_quote():
-
-    response = requests.get('https://api.quotable.io/random')
-
-    quote = response.json()['content']
-
-    print('Quote of the day: "{}"'.format(quote))
-
-
+    quote = random.choice(quotes)
+    logging.info(f'Your quote: {quote}')
+    
 
 dag = DAG(
-
     'welcome_dag',
-
     default_args={'start_date': days_ago(1)},
-
-    schedule_interval='0 23 * * *',
-
+    schedule_interval='0 18 * * *',
     catchup=False
-
 )
-
-
 
 print_welcome_task = PythonOperator(
-
     task_id='print_welcome',
-
     python_callable=print_welcome,
-
     dag=dag
-
 )
-
-
 
 print_date_task = PythonOperator(
-
     task_id='print_date',
-
     python_callable=print_date,
-
     dag=dag
-
 )
 
-
-
-print_random_quote = PythonOperator(
-
+print_random_quote_task = PythonOperator(
     task_id='print_random_quote',
-
     python_callable=print_random_quote,
-
     dag=dag
-
 )
-
-
 
 # Set the dependencies between the tasks
-
-print_welcome_task >> print_date_task >> print_random_quote
+print_welcome_task >> print_date_task >> print_random_quote_task
--- dockerfile
+++ dockerfile
@@ -1,9 +1,21 @@
 FROM apache/airflow:latest
 
+# Switch to the root user to perform system-level operations
 USER root
 
+# Install system-level dependencies
 RUN apt-get update && \
     apt-get -y install git && \
     apt-get clean
 
+# Switch back to the airflow user
 USER airflow
+
+# Set the working directory to the airflow user home directory
+WORKDIR /opt/airflow
+
+# Copy the requirements.txt file into the image
+COPY requirements.txt .
+
+# Install Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
